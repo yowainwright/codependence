b2dbba6cdec91a2371766744078acea079c40164
yowainwright <yowainwright@gmail.com>
2023-04-09 23:40:25 -0700

    feat: init Recipes page

---
+++ page/app/src/pages/Recipes.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Docs } from '../components/Docs'
+import Badges from '../content/badges.mdx'
+
+import Recipes from '../content/recipes.mdx'
+import Footer from '../content/footer.mdx'
+
+export const BasicRecipes = () => (
+  <>
+    <Docs Component={Badges} />
+    <Docs Component={Recipes} />
+    <Docs Component={Footer} />
+  </>
+)
