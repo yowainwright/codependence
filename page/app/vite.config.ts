381e815e72a4aab4075c815f588ebf86256ea1c9
Jeff Wainwright <jeffry@tuta.com>
2025-04-25 13:33:52 -0700

    chore: switches app codeblocks to shiki

--- page/app/vite.config.ts
+++ page/app/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import gfm from 'remark-gfm'
 import slug from 'rehype-slug'
+import rehypePrettyCode from 'rehype-pretty-code'
 
 export default defineConfig(async () => {
   const mdx = await import('@mdx-js/rollup')
@@ -15,7 +16,31 @@ export default defineConfig(async () => {
       mdx.default({
         providerImportSource: '@mdx-js/react',
         remarkPlugins: [gfm],
-        rehypePlugins: [slug],
+        rehypePlugins: [
+          slug,
+          [
+            rehypePrettyCode,
+            {
+              theme: {
+                light: 'github-light',
+                dark: 'dracula',
+              },
+              keepBackground: true,
+              onVisitLine(node) {
+                // Prevent lines from collapsing in `display: grid` mode
+                if (node.children.length === 0) {
+                  node.children = [{ type: 'text', value: ' ' }]
+                }
+              },
+              onVisitHighlightedLine(node) {
+                node.properties.className.push('highlighted')
+              },
+              onVisitHighlightedWord(node) {
+                node.properties.className = ['word']
+              },
+            },
+          ],
+        ],
       }),
     ],
     build: {
