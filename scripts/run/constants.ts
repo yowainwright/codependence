f2d49ea2a723abd9664a033a0776fa827a900f60
Jeff Wainwright <1074042+yowainwright@users.noreply.github.com>
2026-08-18 23:13:31 -0700

    chore(root): updates scripts

--- scripts/run/constants.ts
+++ scripts/run/constants.ts
@@ -1,11 +1,13 @@
 export const TEST_COVERAGE_FLAG = "--coverage";
 export const TEST_COVERAGE_DIR = "coverage";
+export const TEST_COVERAGE_REPORT = `${TEST_COVERAGE_DIR}/lcov.info`;
+export const TEST_COVERAGE_INCLUDE = "src/**/*.ts";
+export const TEST_COVERAGE_EXCLUDE = "tests/**/*.ts";
 export const TEST_COVERAGE_ARGS = [
   "--experimental-test-coverage",
-  "--test-coverage-include=src/**/*.ts",
   "--test-coverage-lines=80",
   "--test-reporter=spec",
   "--test-reporter-destination=stdout",
   "--test-reporter=lcov",
-  `--test-reporter-destination=${TEST_COVERAGE_DIR}/lcov.info`,
+  `--test-reporter-destination=${TEST_COVERAGE_REPORT}`,
 ];
